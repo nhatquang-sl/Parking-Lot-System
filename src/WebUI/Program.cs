75611401d0679fa313adef0342cc0dc8c4bbcafd
Quang Nguyen <quang.nguyen@fmgsuite.com>
2021-09-16 01:59:23 +0700

    Run migration and init database

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,11 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PLS.Application.Common.Interfaces;
+using PLS.Infrastructure.Configurations;
+using PLS.Infrastructure.Persistence;
 
 namespace WebUI
 {
@@ -13,7 +14,28 @@ namespace WebUI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<IApplicationDbContext>();
+
+                    var concreteContext = (ApplicationDbContext)context;
+                    concreteContext.Database.Migrate();
+
+                    var configFactory = scope.ServiceProvider.GetService<IConfigFactory>();
+                    ApplicationDbContextSeed.Initialize(concreteContext, configFactory.GetLevelConfig());
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
